c791ed71d0963fd28b7edb0ed1813283f4db3465
Alvaro Flores <alvaro.flores01@gmail.com>
2017-07-02 18:58:45 -0700

    adds navbar just for the heck of it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+class NavBar extends Component{
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-inverse bg-inverse navbar-toggleable-md navbar-light bg-faded">
+  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <a className="navbar-brand" href="#">Navbar</a>
 
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    );
+  }
+}
 class AddNewTask extends Component {
   constructor(props) {
     super();
@@ -53,7 +85,7 @@ class ToAppList extends Component {
   }
   render() {
     var items = this.props.tasks.map((elem, i ) => {
-      return <li id="tasks" key={i}><span>{elem}</span><button id="delete" onClick={this.remove}>- delete</button></li>
+      return <li id="tasks" key={i}><span>{elem}</span><span className="badge badge-info">Task</span><button id="delete" onClick={this.remove}>- delete</button></li>
     });
     return (
       <div className="tasks">
@@ -87,6 +119,8 @@ class App extends Component {
 }
   render() {
     return (
+      <div>
+      <NavBar />
       <div className="container">
         <div className="content-container">
         <div id="appTitle">
@@ -95,6 +129,7 @@ class App extends Component {
         <AddNewTask updateList={this.updateList}/>
         <ToAppList tasks={this.state.tasks} remove={this.removeTasks}/>
         </div>
+      </div>
       </div>
     );
   }
